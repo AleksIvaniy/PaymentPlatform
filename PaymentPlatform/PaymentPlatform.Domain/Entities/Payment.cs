cb896f4362c51b8e1e7738f486a29729a9f93d6b
alexandr.ivaniy <alexandr.ivaniy@NBKZALAAAE130>
2026-08-14 14:25:31 +0500

    feat: add payment business rules

--- PaymentPlatform/PaymentPlatform.Domain/Entities/Payment.cs
+++ PaymentPlatform/PaymentPlatform.Domain/Entities/Payment.cs
@@ -20,10 +20,40 @@ public class Payment
 
     public Payment(Guid userId, decimal amount)
     {
+        if(userId == Guid.Empty)
+            throw new ArgumentException("UserId cannot be empty", nameof(userId));
+
+        if (amount < 0)
+            throw new ArgumentOutOfRangeException(nameof(amount),"Payment amount must be greater than zero");
+        
         Id = Guid.NewGuid();
         UserId = userId;
         Amount = amount;
         Status = PaymentStatus.Pending;
 
+    }
+
+    public void MarkAsProcessing()
+    {
+        if (Status != PaymentStatus.Pending)
+            throw new InvalidOperationException($"Payment cannot be moved to Processing from {Status}.");
+        
+        Status = PaymentStatus.Processing;
+    }
+
+    public void Complete()
+    {
+        if (Status != PaymentStatus.Processing)
+            throw new InvalidOperationException($"Payment cannot be completed from {Status}.");
+        
+        Status = PaymentStatus.Completed;
+    }
+
+    public void Fail()
+    {
+        if(Status == PaymentStatus.Completed)
+            throw new InvalidOperationException("Completed payment cannot be failed.");
+        
+        Status = PaymentStatus.Failed;
     }
 }
