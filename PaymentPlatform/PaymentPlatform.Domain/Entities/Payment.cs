0216e36b47c7b1d5e1104636fb62b4b6d3f5341e
alexandr.ivaniy <alexandr.ivaniy@NBKZALAAAE130>
2026-08-14 14:02:38 +0500

    feat: add payment domain model

---
+++ PaymentPlatform/PaymentPlatform.Domain/Entities/Payment.cs
@@ -0,0 +1,29 @@
+﻿using PaymentPlatform.Domain.Enums;
+
+namespace PaymentPlatform.Domain.Entities;
+
+public class Payment
+{
+    public Guid Id { get; private set; }
+
+    public Guid UserId { get; private set; }
+
+    public decimal Amount { get; private set; }
+
+    public PaymentStatus Status { get; private set; }
+
+    public DateTime CreatedAt { get; private set; }
+
+    private Payment()
+    {
+    }
+
+    public Payment(Guid userId, decimal amount)
+    {
+        Id = Guid.NewGuid();
+        UserId = userId;
+        Amount = amount;
+        Status = PaymentStatus.Pending;
+
+    }
+}
