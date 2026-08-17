6d21976eeaf30f51c17f02f63f9462233e248f76
AleksIvaniy <165794797+AleksIvaniy@users.noreply.github.com>
2026-08-17 16:42:33 +0500

    refactor: use layer service registration

--- PaymentPlatform/PaymentPlatform.Api/Program.cs
+++ PaymentPlatform/PaymentPlatform.Api/Program.cs
@@ -1,39 +1,23 @@
-using Microsoft.EntityFrameworkCore;
-using PaymentPlatform.Application.Contracts;
-using PaymentPlatform.Application.Interfaces;
-using PaymentPlatform.Application.Services;
-using PaymentPlatform.Infrastructure.Persistence;
-using PaymentPlatform.Infrastructure.Persistence.Repositories;
+using PaymentPlatform.Application;
+using PaymentPlatform.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<PaymentDbContext>(options =>
-{
-    var connectionString =
-        builder.Configuration.GetConnectionString("Postgres");
-
-    options.UseNpgsql(connectionString);
-});
-
-builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
-builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.MapControllers();
 
 app.Run();
-
- 
