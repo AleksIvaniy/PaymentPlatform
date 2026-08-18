19ba116f5a97bf50c13f37646a50d21af946a8d3
alexandr.ivaniy <alexandr.ivaniy@NBKZALAAAE130>
2026-08-18 16:34:13 +0500

    feat: add payment creation endpoint

---
+++ PaymentPlatform/PaymentPlatform.Api/Controllers/PaymentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentPlatform.Application.Contracts;
+using PaymentPlatform.Application.Dto;
+
+namespace PaymentPlatform.Api.Controllers;
+
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class PaymentsController(IPaymentService paymentService) : ControllerBase
+{
+    private readonly IPaymentService _paymentService = paymentService;
+
+
+    [HttpPost]
+    public async Task<ActionResult<CreatePaymentResult>> Create(
+        CreatePaymentCommand command,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await _paymentService.HandleAsync(command, cancellationToken);
+        
+        return Ok(result);
+    }
+}
